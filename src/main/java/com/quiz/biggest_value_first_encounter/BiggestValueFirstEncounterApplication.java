102612a292b2025531f7d99f7203e4b562de2dce
affzkwn <afifzakwanzaili@gmail.com>
2021-10-12 14:36:20 +0800

    changing code style

--- src/main/java/com/quiz/biggest_value_first_encounter/BiggestValueFirstEncounterApplication.java
+++ src/main/java/com/quiz/biggest_value_first_encounter/BiggestValueFirstEncounterApplication.java
@@ -8,22 +8,25 @@ import java.util.*;
 public class BiggestValueFirstEncounterApplication {
 
 	public static void main(String[] args) {
-				// put your code here
 		Scanner scan = new Scanner(System.in);
+		
 		int a = scan.nextInt();
-		int numCount = 0;
 		int[] arr2 = new int[a];
+				
+		for (int j = 0; j < a; j++) {
+			arr2[j] = scan.nextInt();
+		}
+				
 		int maxArr = arr2[0];
-		int len = 0;
-		for (int i = len; i < a; i++) {
-			arr2[i]=scan.nextInt();
+		int numCount = 0;
+				
+		for (int i = 0; i < a; i++) {
 			if (maxArr < arr2 [i]) {
 				maxArr = arr2 [i];
 				numCount = i;
-			} 
-			
-		} System.out.println(numCount);
-				 
-	}
+			} 	
+		} 
 
+		System.out.println(numCount);	 
+	}
 }
