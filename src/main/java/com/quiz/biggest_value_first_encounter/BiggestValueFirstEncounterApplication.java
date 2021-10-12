9638f02bb40dca02bbf9d7b9ef6543621dbc47a5
affzkwn <afifzakwanzaili@gmail.com>
2021-10-12 13:57:22 +0800

    Done but poor code writing

--- src/main/java/com/quiz/biggest_value_first_encounter/BiggestValueFirstEncounterApplication.java
+++ src/main/java/com/quiz/biggest_value_first_encounter/BiggestValueFirstEncounterApplication.java
@@ -11,17 +11,18 @@ public class BiggestValueFirstEncounterApplication {
 				// put your code here
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
-		int num = scan.nextInt();
-		int[] arr2 = new int[num];
+		int numCount = 0;
+		int[] arr2 = new int[a];
 		int maxArr = arr2[0];
 		int len = 0;
-		scan.close();
 		for (int i = len; i < a; i++) {
+			arr2[i]=scan.nextInt();
 			if (maxArr < arr2 [i]) {
 				maxArr = arr2 [i];
+				numCount = i;
 			} 
-			System.out.println(i);
-		} 
+			
+		} System.out.println(numCount);
 				 
 	}
 
